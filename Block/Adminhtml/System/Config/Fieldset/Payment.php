de2f7d467e2e34db9297cf39d9aef874f2c3edea
Allan Wamming Lie <allan.wamming.lie@bambora.com>
2019-10-09 11:28:56 +0200

    Fix for Module configuration disabled if PayPal module not active

--- Block/Adminhtml/System/Config/Fieldset/Payment.php
+++ Block/Adminhtml/System/Config/Fieldset/Payment.php
@@ -38,32 +38,7 @@ class Payment extends \Magento\Config\Block\System\Config\Form\Fieldset
      */
     protected function _getFrontendClass($element)
     {
-        $enabledString = $this->_isPaymentEnabled($element) ? ' enabled' : '';
-        return parent::_getFrontendClass($element) . ' with-button' . $enabledString;
-    }
-
-    /**
-     * Check whether current payment method is enabled
-     *
-     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
-     * @return bool
-     */
-    protected function _isPaymentEnabled($element)
-    {
-        $groupConfig = $element->getGroup();
-        $activityPaths = isset($groupConfig['activity_path']) ? $groupConfig['activity_path'] : [];
-
-        if (!is_array($activityPaths)) {
-            $activityPaths = [$activityPaths];
-        }
-
-        $isPaymentEnabled = false;
-        foreach ($activityPaths as $activityPath) {
-            $isPaymentEnabled = $isPaymentEnabled
-                || (bool)(string)$this->_backendConfig->getConfigDataValue($activityPath);
-        }
-
-        return $isPaymentEnabled;
+        return parent::_getFrontendClass($element) . ' with-button';
     }
 
     /**
@@ -79,13 +54,9 @@ class Payment extends \Magento\Config\Block\System\Config\Form\Fieldset
 
         $groupConfig = $element->getGroup();
 
-        $disabledAttributeString = $this->_isPaymentEnabled($element) ? '' : ' disabled="disabled"';
-        $disabledClassString = $this->_isPaymentEnabled($element) ? '' : ' disabled';
         $htmlId = $element->getHtmlId();
         $html .= '<div class="button-container"><button type="button"' .
-            $disabledAttributeString .
             ' class="button action-configure' .
-            $disabledClassString .
             '" id="' .
             $htmlId .
             '-head" onclick="bamboraToggle.call(this, \'' .
